ad166fcfae5c0839612a58914e7966a52b7eb63e
Yessica Samtani <yessicamsamtani@gmail.com>
2025-10-25 20:00:12 +1000

    Cleaned up

--- recognition/oasis_unet_s4809289/train.py
+++ recognition/oasis_unet_s4809289/train.py
@@ -11,6 +11,7 @@ def train(model, train_loader, validation_loader, validation_tensor_set, epochs=
     model.to(device)
     
     loss_function = DiceLoss()
+    # bce_function = nn.BCELoss()
     optimizer = optim.Adam(model.parameters(), lr=lr)
 
     train_loss = []
@@ -28,8 +29,8 @@ def train(model, train_loader, validation_loader, validation_tensor_set, epochs=
             outputs = model(image) # returns softmax output format
 
             # Loss function (DiceLoss) on all classes
+            # loss = 0.5 * bce_function(outputs, mask) + 0.5 * loss_function(outputs, mask)
             loss = loss_function(outputs, mask)
-
             loss.backward()
             optimizer.step() # gradient descent
 
@@ -46,6 +47,7 @@ def train(model, train_loader, validation_loader, validation_tensor_set, epochs=
             for image, mask in validation_loader:
                 image, mask = image.to(device), mask.to(device)
                 outputs = model(image)
+                # val_loss = 0.5 * bce_function(outputs, mask) + 0.5 * loss_function(outputs, mask)
                 val_loss = loss_function(outputs, mask)
                 epoch_val_loss += val_loss.item()
 
@@ -60,3 +62,4 @@ def train(model, train_loader, validation_loader, validation_tensor_set, epochs=
 
     print("Training model complete.")
     return train_loss, validation_loss
+
