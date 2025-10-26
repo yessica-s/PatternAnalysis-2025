ef9745a8ac6662fa40c3e15e435025d9c8cffd67
Yessica Samtani <yessicamsamtani@gmail.com>
2025-10-26 11:14:06 +1000

    Reverted to dice loss, removed BCE

--- recognition/oasis_unet_s4809289/predict.py
+++ recognition/oasis_unet_s4809289/predict.py
@@ -29,16 +29,22 @@ if __name__ == "__main__":
     model.eval()
     dice_loss = DiceLoss()
     test_loss = 0
+    test_coefficients = 0
 
     with torch.no_grad():
         for image, mask in test_loader:
             image, mask = image.to(device), mask.to(device)
             output = model(image)
-            loss = dice_loss(output, mask)
+            loss = dice_loss(output, mask) # returns dice loss
             test_loss += loss.item()
 
+            dice_coeffcient = 1 - loss.item() # get dice coefficient
+            test_coefficients += dice_coeffcient
+
     avg_test_loss = test_loss / len(test_loader)
+    avg_test_coefficient = test_coefficients / len(test_loader)
     print(f"Average Test Dice Loss: {avg_test_loss:.4f}")
+    print(f"Average Test Dice Coefficient: {avg_test_coefficient:.4f}")
 
     # Visualize predictions
     print("Visualizing predictions on test samples...")
